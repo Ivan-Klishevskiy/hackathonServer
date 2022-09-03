9d11664b84b6931a3ef774d2d125a5a53d2ff9de
KLISHEVSKIY\Kliis <i.klishevskiy@mail.ru>
2022-09-03 18:47:01 +0300

    fix user response

---
+++ src/main/java/by/tms/hackathonserver/dto/response/UserDto.java
@@ -0,0 +1,34 @@
+package by.tms.hackathonserver.dto.response;
+
+import by.tms.hackathonserver.model.Product;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Column;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+public class UserDto {
+    private long id;
+
+    private String username;
+
+    @JsonIgnore
+    private String password;
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String phoneNumber;
+
+    private List<Product> products;
+}
--- src/main/java/by/tms/hackathonserver/service/UserService.java
+++ src/main/java/by/tms/hackathonserver/service/UserService.java
@@ -1,11 +1,17 @@
 package by.tms.hackathonserver.service;
 
+import by.tms.hackathonserver.dto.response.ProductResponse;
+import by.tms.hackathonserver.dto.response.UserDto;
+import by.tms.hackathonserver.model.Product;
 import by.tms.hackathonserver.model.User;
 import by.tms.hackathonserver.model.UserStatus;
+import by.tms.hackathonserver.repository.ProductRepository;
 import by.tms.hackathonserver.repository.UserRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,12 +20,22 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
 
-    public User getUser(String email, String password){
+    @Autowired
+    private ModelMapper mapper;
+
+    public UserDto getUser(String email, String password) {
         Optional<User> tempUser = userRepository.findByEmailAndPassword(email, password);
-        if(tempUser.isPresent()){
-            return tempUser.get();
-        }else{
+        if (tempUser.isPresent()) {
+            User foundUser = tempUser.get();
+            List<Product> userProducts = productRepository
+                    .findAllByCreatorUsername(foundUser.getUsername()).get();
+            UserDto userDto = mapper.map(foundUser, UserDto.class);
+            userDto.setProducts(userProducts);
+            return userDto;
+        } else {
             throw new RuntimeException("User not found");
         }
     }
@@ -27,18 +43,18 @@ public class UserService {
     public User save(User user) {
         Optional<User> dataUser = Optional.ofNullable(userRepository.findByUsername(user.getUsername()));
 
-        if(dataUser.isPresent()){
+        if (dataUser.isPresent()) {
             throw new RuntimeException("User already exist");
         }
         return userRepository.save(user);
     }
 
-    public void delete(String username){
+    public void delete(String username) {
         Optional<User> dataUser = Optional.ofNullable(userRepository.findByUsername(username));
 
-        if(dataUser.isPresent()){
+        if (dataUser.isPresent()) {
             userRepository.delete(dataUser.get());
-        }else {
+        } else {
             throw new RuntimeException("user not found");
         }
     }
