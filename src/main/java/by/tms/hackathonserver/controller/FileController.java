c1a357ed92165a11039cd91ce8bfd86bc16c777f
KLISHEVSKIY\Kliis <i.klishevskiy@mail.ru>
2022-09-03 21:07:54 +0300

    fix user response

--- src/main/java/by/tms/hackathonserver/controller/FileController.java
+++ src/main/java/by/tms/hackathonserver/controller/FileController.java
@@ -1,19 +1,42 @@
-//package by.tms.hackathonserver.controller;
-//
-//
-//import by.tms.hackathonserver.model.Product;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//import org.springframework.web.multipart.MultipartFile;
-//
-//@RestController
-//@RequestMapping("/file")
-//public class FileController {
-//
-//    @PostMapping("/save")
-//    public ResponseEntity<?> save(@RequestParam("file") MultipartFile file) {
-//        Product savedProduct = productService.save(product);
-//        return new ResponseEntity<>(savedProduct, HttpStatus.OK);
-//    }
-//}
+package by.tms.hackathonserver.controller;
+
+
+import by.tms.hackathonserver.model.Product;
+import by.tms.hackathonserver.service.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.awt.image.BufferedImage;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@RestController
+@RequestMapping("/file")
+public class FileController {
+
+    @Autowired
+    private FileService fileService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file) {
+        fileService.uploadFile(file);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/image/{fileName}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<Resource> image(@PathVariable String fileName) {
+        ByteArrayResource byteArrayResource = fileService.loadFile(fileName);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentLength(byteArrayResource.contentLength())
+                .body(byteArrayResource);
+    }
+}
--- src/main/java/by/tms/hackathonserver/service/FileService.java
+++ src/main/java/by/tms/hackathonserver/service/FileService.java
@@ -1,14 +1,46 @@
-//package by.tms.hackathonserver.service;
-//
-//import org.springframework.stereotype.Service;
-//import org.springframework.web.multipart.MultipartFile;
-//
-//import java.io.File;
-//
-//@Service
-//public class FileService {
-//
-//    public void uploadFile(MultipartFile file){
-//        file.transferTo(new File(""));
-//    }
-//}
+package by.tms.hackathonserver.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Service
+public class FileService {
+
+    //
+//    private String fileDir;
+    @Value("${dir.uploadFiles}")
+    private String path;
+
+    public void uploadFile(MultipartFile file) {
+
+        try {
+            file.transferTo(new File(path + file.getOriginalFilename()));
+        } catch (IOException e) {
+            throw new RuntimeException("Error uploadFile");
+        }
+    }
+
+    public ByteArrayResource loadFile(String fileName) {
+        final ByteArrayResource inputStream;
+        System.out.println(path + File.separator + fileName + ".jpg");
+        try {
+            inputStream = new ByteArrayResource(Files.readAllBytes(Paths.get(
+                    path + File.separator + fileName + ".jpg"
+            )));
+        } catch (IOException e) {
+            throw new RuntimeException("Fail");
+        }
+        return inputStream;
+    }
+}
